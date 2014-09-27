ece02b34e2122787438049478b85e97da56551c7
Ben Norris <ben@bsn.io>
2014-09-27 10:27:13 -0600

    Added toolbar with buttons

--- OptionsToolbar/OTViewController.m
+++ OptionsToolbar/OTViewController.m
@@ -21,25 +21,45 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (IBAction)mail:(id)sender {
-    
-    MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
+- (IBAction)sendMail:(id)sender {
+    MFMailComposeViewController *mailViewController = [MFMailComposeViewController new];
     mailViewController.mailComposeDelegate = self;
-    
-    [self presentViewController:mailViewController animated:YES completion:nil];
-    
+    [mailViewController setSubject:@"Send me an email"];
+    // Body doesn't get set although subject does
+    [mailViewController setMessageBody:@"I love that this is working now!" isHTML:NO];
+    if ([MFMailComposeViewController canSendMail]) {
+        [self presentViewController:mailViewController animated:YES completion:nil];
+    }
 }
 
 - (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
-    
-    [self dismissViewControllerAnimated:YES completion:nil];
-    
+    [controller dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (IBAction)red:(id)sender {
-
+- (IBAction)changeToRed:(id)sender {
     self.view.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)changeToBlue:(id)sender {
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (IBAction)newViewController:(id)sender {
+    UIViewController *newView = [UIViewController new];
+    newView.view.backgroundColor = [UIColor greenColor];
+    // My button is still not showing up
+    UIButton *dismissButton = [[UIButton alloc] initWithFrame:CGRectMake(15, 200, 100, 30)];
+    [dismissButton setTitle:@"Record Time" forState:UIControlStateNormal];
+    [dismissButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [dismissButton addTarget:self action:@selector(dismissNewView:) forControlEvents:UIControlEventTouchUpInside];
+    [newView.view addSubview:dismissButton];
+    [self.view addSubview:dismissButton];
+    
+    [self presentViewController:newView animated:YES completion:nil];
+}
+
+- (void)dismissNewView:(UIButton *) button {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
